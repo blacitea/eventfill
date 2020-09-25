bca116a405ce5f3203e9dec9b997a8e19373123f
Marisa Doig <mcr.doig@gmail.com>
2020-09-25 17:34:25 +0000

    Rubocop autocorrect of boilerplate

--- app/controllers/api/tests_controller.rb
+++ app/controllers/api/tests_controller.rb
@@ -1,7 +1,11 @@
-class Api::TestsController < ApplicationController
-  def index
-    render :json => {
-      message: "hello!"
-    }
+# frozen_string_literal: true
+
+module Api
+  class TestsController < ApplicationController
+    def index
+      render json: {
+        message: 'hello!'
+      }
+    end
   end
 end
