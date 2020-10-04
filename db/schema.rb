cbd5707ff389dc425b5659191a6b3ef52c5f7e6d
Marisa Doig <mcr.doig@gmail.com>
2020-10-04 02:08:35 +0000

    Migration to remove time from gigs

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_201_002_015_137) do
+ActiveRecord::Schema.define(version: 20_201_004_020_526) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 20_201_002_015_137) do
   create_table 'gigs', force: :cascade do |t|
     t.integer 'talent_profile_id', null: false
     t.integer 'event_id', null: false
-    t.datetime 'time', null: false
     t.text 'description'
     t.boolean 'accepted'
     t.boolean 'rejected'
