5bedd2b0769a3e84609b60e857ce95f48c3aedca
Angel <blacitea@gmail.com>
2020-10-05 22:16:28 -0700

    add seeds for event and talents

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,10 +18,9 @@ Gig.destroy_all
 Registration.destroy_all
 
 Genre.create!(name: 'Anime and Manga')
-Genre.create!(name: 'Collectable Card Game')
+Genre.create!(name: 'Table Top Games')
 Genre.create!(name: 'Comics')
 Genre.create!(name: 'Handcraft')
-Genre.create!(name: 'Tabletop Games')
 Genre.create!(name: 'Video Games')
 
 Location.create!(name: 'Vancouver')
@@ -101,12 +100,85 @@ Message.create!(sender_id: 5, recipient_id: 4, content: 'I really had fun.')
 
 
 
-Event.create!(user_id: 1, genre_id: 1, location_id: 1, image_url: 'https://i.pcmag.com/imagery/lineups/06PiprC5eiYhlpaNS6N0sbP-1.1569492872.fit_lim.fit_lim.size_956x99999.jpg', name: 'GameCon Vancouver 2020', start: '2020-10-13 12:00:00', end: '2020-10-13 22:00:00', max_attendees: 200, description: 'Come to this!', accepting_talent: true)
-Event.create!(user_id: 1, genre_id: 3, location_id: 1, image_url: 'https://www.thespruce.com/thmb/Pot7VQ8sCBiR4virPcehRZYbGq8=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/-assortment-of-scrapbooking-tools-including-colored-paper--pens--and-scissors-click-below-for-more-of-my-scrapbooking-and-arts-and-crafts-images--936822170-ed08f5a6b9d74a0191f16a6f3dd5a272.jpg', name: 'CraftCon Vancouver 2020', start: '2020-09-20 10:00:00', end: '2020-09-20 19:00:00', max_attendees: 120, description: 'Come to this also!', accepting_talent: false)
+Event.create!(user_id: 1, genre_id: 1, location_id: 1, image_url: '/images/event_1.jpg', name: 'Anime Revolution 2020', start: '2020-07-31 12:00:00', end: '2020-08-03 22:00:00', max_attendees: 200, description: 'Join us for another year of Anime, Anime, and more Anime! Anirevo Summer is officially booked for July 31st to August 2nd 2020 at the Vancouver Convention Centre (West Building)!', accepting_talent: true)
 
-TalentProfile.create!(user_id: 1, genre_id: 1, location_id: 1, image_url: 'https://image.shutterstock.com/mosaic_250/2797510/600200732/stock-photo-portrait-of-happy-fashionable-handsome-man-in-jeans-shirt-crossing-hands-and-look-at-camera-600200732.jpg', description: 'Game designer', personal_link: 'me@games.com', open_for_booking: true, name: 'JimothyGames')
-TalentProfile.create!(user_id: 1, genre_id: 2, location_id: 1, image_url: 'https://image.shutterstock.com/mosaic_250/2797510/600200732/stock-photo-portrait-of-happy-fashionable-handsome-man-in-jeans-shirt-crossing-hands-and-look-at-camera-600200732.jpg', description: 'Comic artist', personal_link: 'me@comics.com', open_for_booking: true, name: 'JimothyComics')
-TalentProfile.create!(user_id: 3, genre_id: 3, location_id: 3, image_url: 'https://static01.nyt.com/images/2019/09/04/business/04chinaclone-01/04chinaclone-01-videoSixteenByNineJumbo1600.jpg', description: 'Crafter', personal_link: 'me@crafts.com', open_for_booking: false, name: 'Prudence Craftwell')
+Event.create!(user_id: 6, genre_id: 1, location_id: 1, image_url: '/images/event_2.jpg', name: 'Celebrate Jojo 2020', start: '2020-11-20 10:00:00', end: '2020-11-21 19:00:00', max_attendees: 50, description: 'JoJo\'s Bizarre Adventure is a Japanese manga series written and illustrated by Hirohiko Araki. Come and experience the madness of Jojo\'s adventure world.', accepting_talent: false)
+
+Event.create!(user_id: 3, genre_id: 1, location_id: 5, image_url: '/images/event_3.jpeg', name: 'Demon Slayer', start: '2021-01-01 10:00:00', end: '2021-01-02 19:00:00', max_attendees: 50, description: 'Cosplayers welcome! Join us in this event to share our love and watch the 1st episode of season 3 together!', accepting_talent: false)
+
+Event.create!(user_id: 9, genre_id: 1, location_id: 2, image_url: '/images/event_4.jpg', name: 'Volleyball Love!', start: '2020-10-21 10:00:00', end: '2020-10-23 19:00:00', max_attendees: 20, description: 'Celebrating the new season, we are going to have some volleyball match and watch the show after our games!', accepting_talent: false)
+
+
+Event.create!(user_id: 4, genre_id: 2, location_id: 1, image_url: '/images/event_5.jpg', name: 'Friday Magic!', start: '2020-10-09 10:00:00', end: '2020-10-10 19:00:00', max_attendees: 40, description: 'Come join us to draft a deck and battle each other! We do this every Friday!', accepting_talent: false)
+
+Event.create!(user_id: 4, genre_id: 2, location_id: 1, image_url: '/images/event_6.jpg', name: 'Friday Magic!', start: '2020-10-16 10:00:00', end: '2020-10-17 19:00:00', max_attendees: 40, description: 'Come join us to draft a deck and battle each other! We do this every Friday!', accepting_talent: false)
+
+Event.create!(user_id: 11, genre_id: 2, location_id: 3, image_url: '/images/event_7.jpeg', name: 'EDH Championship', start: '2020-11-10 10:00:00', end: '2020-11-11 19:00:00', max_attendees: 50, description: 'Bring your favourite commander deck and we shall see who\'s the best!', accepting_talent: false)
+
+Event.create!(user_id: 13, genre_id: 2, location_id: 2, image_url: '/images/event_8.jpeg', name: 'Stardard 101!', start: '2020-12-02 10:00:00', end: '2020-12-03 19:00:00', max_attendees: 50, description: 'Want to learn about Magic The Gathering? Join us for some basic and get a starter deck to play with!', accepting_talent: false)
+
+
+
+
+Event.create!(user_id: 8, genre_id: 3, location_id: 5, image_url: '/images/event_9.jpeg', name: 'Marvel Universe!', start: '2020-12-04 10:00:00', end: '2020-12-05 19:00:00', max_attendees: 100, description: 'Hulk? Deadpool? Ghost? Groot? Daredevil? You name it, we have it! Come check it out!', accepting_talent: false)
+
+Event.create!(user_id: 4, genre_id: 3, location_id: 3, image_url: '/images/event_10.jpeg', name: 'Amazing! Spiderman!', start: '2020-12-12 10:00:00', end: '2020-12-13 19:00:00', max_attendees: 100, description: 'Spiderman, Spiderman!
+Does whatever a spider he can.
+Spins a web any size,
+Catches thieves, just like flies.
+Look out! Here comes the Spiderman!', accepting_talent: false)
+
+Event.create!(user_id: 5, genre_id: 3, location_id: 4, image_url: '/images/event_11.jpeg', name: 'Justice!', start: '2020-12-22 10:00:00', end: '2020-12-23 19:00:00', max_attendees: 200, description: 'For decades, the Justice League has saved humanity from the worst threats it has ever faced, from alien warlords to ancient demons and powerful sorcerers, to the God of Evil himself, Darkseid. And every time, they have overcome the odds and saved the human race.', accepting_talent: false)
+
+Event.create!(user_id: 3, genre_id: 3, location_id: 4, image_url: '/images/event_12.jpeg', name: 'Snoopy Party!', start: '2020-12-07 10:00:00', end: '2020-12-09 19:00:00', max_attendees: 20, description: 'Snoopy is a loyal, imaginative and good-natured beagle who is prone to imagining fantasy lives, including being an author, a college student known as "Joe Cool", an attorney and a British World War I flying ace. He is perhaps best known in this last persona, wearing an aviator\'s helmet and goggles and a scarf while carrying a swagger stick (like a stereotypical British Army officer of World War I and II).', accepting_talent: false)
+
+
+
+
+Event.create!(user_id: 4, genre_id: 4, location_id: 1, image_url: '/images/event_13.jpeg', name: 'Cute Kittens!', start: '2020-10-11 10:00:00', end: '2020-10-12 19:00:00', max_attendees: 10, description: 'My first handcrafting event! Come see my art and learn how to make yours.', accepting_talent: false)
+
+Event.create!(user_id: 1, genre_id: 4, location_id: 2, image_url: '/images/event_14.jpeg', name: 'Knitting fun!', start: '2020-11-07 10:00:00', end: '2020-11-09 19:00:00', max_attendees: 20, description: 'I am going to hold a small knitting class, if you know your basic and want to get better, bring your yarn and needle!', accepting_talent: false)
+
+Event.create!(user_id: 1, genre_id: 4, location_id: 3, image_url: '/images/event_15.jpeg', name: 'Perfect Fit', start: '2020-11-29 10:00:00', end: '2020-11-30 19:00:00', max_attendees: 5, description: 'Spend your weekend with us and we will make sure you walk away with a pair of perfect fits.', accepting_talent: false)
+
+
+
+
+Event.create!(user_id: 12, genre_id: 5, location_id: 1, image_url: '/images/event_16.jpeg', name: 'Ranked Battles!', start: '2020-10-15 10:00:00', end: '2020-10-17 19:00:00', max_attendees: 20, description: 'Pokémon Sword and Pokémon Shield Ranked Battles October Season Is Here
+Battle your fellow Trainers to move up the leaderboard and earn helpful in-game rewards.', accepting_talent: false)
+
+Event.create!(user_id: 12, genre_id: 5, location_id: 2, image_url: '/images/event_17.jpeg', name: 'Squad Strike Teams 3 vs 3!', start: '2020-10-26 10:00:00', end: '2020-10-27 19:00:00', max_attendees: 50, description: 'Gather your team and complete in this 3 vs 3 championship!', accepting_talent: false)
+
+Event.create!(user_id: 3, genre_id: 5, location_id: 4, image_url: '/images/event_18.jpeg', name: 'ASL Season 10 Final!', start: '2020-12-05 10:00:00', end: '2020-12-06 19:00:00', max_attendees: 200, description: 'The AfreecaTV Starleague Season 10 (아프리카TV 스타리그 시즌10) is coming to an end! Come watch the final and cheer for your favourite race!', accepting_talent: false)
+
+Event.create!(user_id: 4, genre_id: 5, location_id: 1, image_url: '/images/event_19.jpeg', name: 'Van TI', start: '2020-11-25 10:00:00', end: '2020-11-27 19:00:00', max_attendees: 100, description: 'Are you disappointed that TI was cancelled? We too! Come play and we will have our Van TI instead!', accepting_talent: false)
+
+
+
+
+
+TalentProfile.create!(user_id: 1, genre_id: 1, location_id: 1, image_url: '/images/talent_1.jpeg', description: 'Speed runner! I am open for charity events, contact me!  Aenean viverra est in blandit gravida. Integer bibendum vestibulum ante, quis scelerisque magna vulputate quis. Nulla purus nulla, feugiat ac ante quis, rhoncus sagittis nunc. Nam condimentum velit non sapien hendrerit, vestibulum semper augue facilisis. ', personal_link: 'https://www.twitch.tv/mizkif', open_for_booking: true, name: 'Jimothy Speed')
+
+TalentProfile.create!(user_id: 2, genre_id: 2, location_id: 2, image_url: '/images/talent_2.jpeg', description: 'Need a MC for your event? I can keep the crowd going~ Integer tincidunt leo non augue vulputate tempor. Nam a pretium ipsum. Duis vehicula sodales tortor a suscipit. Maecenas nisi lacus, porttitor et gravida venenatis, viverra at nulla. Phasellus a sapien purus. Vivamus suscipit mollis lacus, ut venenatis risus porta et. Nam ut metus vitae lectus lacinia gravida eu ut nunc.', personal_link: 'https://www.twitch.tv/sykkuno', open_for_booking: true, name: 'Kyuuno')
+
+TalentProfile.create!(user_id: 2, genre_id: 1, location_id: 1, image_url: '/images/talent_3.jpeg', description: 'Want some performance game demo? I am your player! Maecenas et faucibus ante, non ornare odio. Nullam vitae risus dolor. Sed congue diam interdum est porta, in ullamcorper enim scelerisque. Aenean bibendum semper augue a vestibulum. Pellentesque accumsan eros eros, eget ornare neque scelerisque eu. Donec ut pulvinar felis. ', personal_link: 'https://www.twitch.tv/esl_csgo', open_for_booking: true, name: 'CSGO')
+
+TalentProfile.create!(user_id: 3, genre_id: 3, location_id: 1, image_url: '/images/talent_4.jpeg', description: 'Game designer! I like to talk about video game and developers.  Nam sed placerat ligula, at mattis tortor. Integer rutrum tellus ornare finibus ullamcorper. Pellentesque convallis quam non tellus ultrices pulvinar. In gravida vehicula neque, sed condimentum sapien tempus in. Integer aliquet lobortis arcu ut pulvinar. Curabitur at ipsum tristique augue hendrerit faucibus eu eu leo.', personal_link: 'https://twitter.com/elbaz_eran', open_for_booking: true, name: 'Code Games')
+
+TalentProfile.create!(user_id: 4, genre_id: 4, location_id: 1, image_url: '/images/talent_5.jpeg', description: 'You want me for your event, you know it. Fusce laoreet et eros sed accumsan. Integer finibus sit amet ex sed iaculis. Suspendisse malesuada, diam eget imperdiet consequat, est elit varius nisl, consequat imperdiet magna massa in lorem. Nulla congue enim et orci pellentesque, sit amet sagittis purus faucibus. ', personal_link: 'https://www.youtube.com/channel/UC4YaOt1yT-ZeyB0OmxHgolA', open_for_booking: true, name: 'AiChan')
+
+TalentProfile.create!(user_id: 5, genre_id: 2, location_id: 2, image_url: '/images/talent_6.jpeg', description: 'I make monster music that rock your events! Pellentesque pulvinar odio quis nisi tempor, ac tristique quam ullamcorper. Ut posuere leo in massa ornare, ut elementum magna cursus. Donec in magna eu neque posuere accumsan. Sed sit amet ante sit amet sapien mattis euismod. Suspendisse ut augue accumsan, vulputate eros vitae, hendrerit tortor.', personal_link: 'https://soundcloud.com/monstercat', open_for_booking: true, name: 'MonsCat')
+
+TalentProfile.create!(user_id: 5, genre_id: 1, location_id: 2, image_url: '/images/talent_7.jpeg', description: 'Hololive-EN artist. Sharrrrrrrrrk! Duis scelerisque felis non nunc tristique efficitur. Donec congue a purus eget gravida. Etiam commodo arcu eget nisl porttitor gravida. Phasellus ut vulputate ex. Donec iaculis libero est, eu vehicula dui interdum vitae. Duis in quam in nisi dictum molestie et a leo. Aenean venenatis purus tellus, ac ', personal_link: 'https://www.youtube.com/channel/UCoSrY_IQQVpmIRZ9Xf-y93g', open_for_booking: true, name: 'Gawr Gura')
+
+TalentProfile.create!(user_id: 7, genre_id: 2, location_id: 3, image_url: '/images/talent_8.jpeg', description: 'Want some music for your event? I am your best pick! feugiat ac ante quis, rhoncus sagittis nunc. Nam condimentum velit non sapien hendrerit, vestibulum semper augue facilisis. Donec nec metus eget erat cursus feugiat. Aenean feugiat odio sit amet lacus convallis, mollis gravida nibh iaculis. Suspendisse vestibulum laoreet magna, id interdum dolor volutpat non. Integer lectus tortor, ultricies sed eros ac, placerat commodo erat.', personal_link: 'https://soundcloud.com/travisscott-2', open_for_booking: true, name: 'Travis S')
+
+TalentProfile.create!(user_id: 8, genre_id: 3, location_id: 4, image_url: '/images/talent_9.jpeg', description: 'I am crazy about anime and manga, are you not? Cosplay talent. Nam a pretium ipsum. Duis vehicula sodales tortor a suscipit. Maecenas nisi lacus, porttitor et gravida venenatis, viverra at nulla. Phasellus a sapien purus. Vivamus suscipit mollis lacus, ut venenatis risus porta et. Nam ut metus vitae lectus lacinia gravida eu ut nunc.', personal_link: 'https://twitter.com/TastyTasteless', open_for_booking: true, name: 'T@astLeSS')
+
+TalentProfile.create!(user_id: 1, genre_id: 2, location_id: 5, image_url: '/images/talent_10.jpeg', description: 'I draw small robots and they make people happy! Sed congue diam interdum est porta, in ullamcorper enim scelerisque. Aenean bibendum semper augue a vestibulum. Pellentesque accumsan eros eros, eget ornare neque scelerisque eu. Donec ut pulvinar felis. Phasellus erat ex, congue ac ullamcorper in, vehicula quis orci. ', personal_link: 'https://twitter.com/smolrobots', open_for_booking: true, name: 'Small RObots')
+
+
+
 
 Gig.create!(talent_profile_id: 2, event_id: 1, description: 'Performing, ya!', accepted: true, rejected: nil)
 Gig.create!(talent_profile_id: 3, event_id: 2, description: 'Drawings galore~', accepted: nil, rejected: true)
