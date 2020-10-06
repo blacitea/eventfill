f224036d4b8e6a5f7fd55ef4807b9a8ab4b60874
Quinn <quinnvoker@gmail.com>
2020-10-06 15:44:26 -0700

    db resets between tests now possible

--- config/initializers/cypress_rails_initializer.rb
+++ config/initializers/cypress_rails_initializer.rb
@@ -4,12 +4,15 @@ Rails.application.load_tasks unless defined?(Rake::Task)
 
 CypressRails.hooks.before_server_start do
   Rake::Task["db:reset"].invoke
+  puts "before server start"
 end
 
 CypressRails.hooks.after_transaction_start do
 end
 
 CypressRails.hooks.after_state_reset do
+  puts "state reset"
+  Rake::Task["db:reset"].invoke
 end
 
 CypressRails.hooks.before_server_stop do
