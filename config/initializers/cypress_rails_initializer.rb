d935ea593a41481ceb5abeca1599a560878ae0f7
Quinn <quinnvoker@gmail.com>
2020-10-06 15:46:29 -0700

    removed console logs

--- config/initializers/cypress_rails_initializer.rb
+++ config/initializers/cypress_rails_initializer.rb
@@ -4,14 +4,12 @@ Rails.application.load_tasks unless defined?(Rake::Task)
 
 CypressRails.hooks.before_server_start do
   Rake::Task["db:reset"].invoke
-  puts "before server start"
 end
 
 CypressRails.hooks.after_transaction_start do
 end
 
 CypressRails.hooks.after_state_reset do
-  puts "state reset"
   Rake::Task["db:reset"].invoke
 end
 
